4efe100c93665782e86f9bb3906c3f7667491008
github username <github email address>
2016-09-29 12:51:58 -0400

    All Tests Green

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  attr_reader :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    @roster.has_key?(grade) ? roster[grade] << student : roster[grade] = [student]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grades, student_names_arr|
+      @roster[grades].sort!
+    end
+    @roster.sort.to_h
+  end
+
+end
